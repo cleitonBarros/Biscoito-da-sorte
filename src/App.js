68ff40301e339f75337d803062977e106f07b555
cleitonBarros <48657479+cleitonBarros@users.noreply.github.com>
2022-02-16 11:50:08 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,45 +4,58 @@ import './estilo.css'
 
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            texto:''
+            numero:0,
+            botaotxt:'go'
         }
-        this.quebraBiscoito =  this.quebraBiscoito.bind(this)
-        this.frases = [`Je pense, donc je suis.(René Descartes)`,
-        `Connais-toi toi-même.`,
-       ` L'homme est un pont, non une fin. (Friedrich Nietzsche)`,
-        `La liberté est le droit de faire tout ce que les lois permettent. (Montesquieu)`, 
-        `Rien de grand ne s'est fait dans le monde sans passion(Hegel)`]
+        this.timer = null
+        this.go =  this.go.bind(this)
+        this.clear = this.clear.bind(this)
     }
-    quebraBiscoito(){
-        let state =  this.state;
-        let rand = Math.floor(Math.random() * this.frases.length);
-        state.texto = this.frases[rand];
-
+    go(){
+        let state =  this.state
+        if(this.timer !== null){
+            clearInterval(this.timer)
+            this.timer = null;
+            state.botaotxt = 'go'
+        } else{
+        this.timer = setInterval(()=>{
+            let state =  this.state;
+            state.numero += 0.1
+            this.setState(state)
+        },100)
+        state.botaotxt = 'Pause'
+    }
         this.setState(state)
+
+    }
+    clear(){
+        if(this.timer !== null){
+            clearInterval(this.timer)
+            this.timer = null;
+            
+        }
+        let state =  this.state;
+        state.numero = 0;
+        state.botaotxt = 'Go'
+        this.setState(state);
 
     }
     render(){
         return(
             <div className="Container">
-              <img src={require('./assets/biscoito.png') } className="img"></img>
-              <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
-              <h3 className="texto">{this.state.texto}</h3>
+              <img scr={require('./assets/cronometro.png') }className="img"></img>
+              <a className="timer">{this.state.numero.toFixed(1)}</a>
+              <div className="areabtn">
+                  <a className="btn" onClick={this.go}>{this.state.botaotxt}</a>
+                  <a className="btn" onClick={this.clear}>Clear</a>
+              </div>
             </div>
         )
     }
 }
 
-class Botao extends Component{
-    render(){
-        return(
-            <div>
-                <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
-          </div>
-        )
-    }
-}
 
 export default App;
